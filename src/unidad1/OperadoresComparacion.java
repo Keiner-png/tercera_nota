3a5ba58f0c0b00eaf4dd167c58ad0115b5e9b6ff
Keiner-png <kjdurana@ufpso.edu.co>
2025-09-01 09:40:58 -0500

    Operadores de comparacion numerica

---
+++ src/unidad1/OperadoresComparacion.java
@@ -0,0 +1,15 @@
+package unidad1;
+
+public class OperadoresComparacion {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("¿El numero 1 es igual al numero 2? " + (num1 == num2));
+        System.out.println("¿El numero 1 es diferente de numero 2? " + (num1 != num2));
+        System.out.println("¿El numero 1 es mayor que numero 2? " + (num1 > num2));
+        System.out.println("¿El numero 1 es menor que numero 2? " + (num1 < num2));
+        System.out.println("¿El numero 1 es mayor o igual que numero 2? " + (num1 >= num2));
+        System.out.println("¿El numero 1 es menor o igual que numero 2? " + (num1 <= num2));
+    }
+}
