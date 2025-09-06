d02d7db633659172611c1fd3525bb6b92f8ee706
Keiner-png <kjdurana@ufpso.edu.co>
2025-09-06 07:14:16 -0500

    Ejercicio basura

---
+++ src/unidad1/If_Else.java
@@ -0,0 +1,11 @@
+package unidad1;
+
+import java.util.Scanner;
+
+public class If_Else {
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+
+        consola.close();
+    }
+}
---
+++ src/unidad1/NumerosConFor.java
@@ -0,0 +1,22 @@
+package unidad1;
+
+public class NumerosConFor {
+    public static void main(String[] args) {
+        int num1 = 3 ;
+        int num2 = 5 ; 
+        int num3 = 1 ;
+        int numMax ;
+        for( int i = 0 ; i < 5 ; i++){
+            if (num1 > num2 && num1 > num3) {
+                numMax = num1;
+            } else if(num2 > num1 && num2 > num3) {
+                numMax = num2;
+            } else 
+             numMax = num3;
+
+                
+
+        }
+
+    }
+}
